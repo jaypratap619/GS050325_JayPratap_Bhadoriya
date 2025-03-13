1240d9168ea5c49275391828f11663ca65b56b28
jay bhadoriya <18301@iiitu.ac.in>
2025-03-14 02:07:30 +0530

    import issue and lint error of Sign Up page

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { reorderCalculations } from "./redux/calculationsSlice";
 import { reOrderPlans } from "./redux/planningSlice";
 import * as XLSX from "xlsx";
 import Login from "./components/Login";
-import SignUp from "./components/SignUp";
+import SignUp from "./components/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
